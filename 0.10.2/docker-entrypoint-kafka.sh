1ac124823983de5964ad8bdcb099e3e65318349c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-04-19 18:09:27 +0200

    Don't use runit

--- 0.10.2/docker-entrypoint-kafka.sh
+++ 0.10.2/docker-entrypoint-kafka.sh
@@ -3,4 +3,8 @@
 # Fix /tmp/kafka-logs permissions
 chown -R kafka /tmp/kafka-logs
 
-runsvdir -P /etc/service/
+echo
+echo "== /kafka/config/server.properties =="
+cat /kafka/config/server.properties
+
+exec gosu kafka /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
