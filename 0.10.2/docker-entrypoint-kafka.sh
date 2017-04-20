408142e26515a4ef7c56e967050e9b21b67a1779
Mickaël Canévet <mickael.canevet@camptocamp.com>
2017-04-20 08:10:26 +0200

    Don't use confd to configure server.properties

--- 0.10.2/docker-entrypoint-kafka.sh
+++ 0.10.2/docker-entrypoint-kafka.sh
@@ -3,6 +3,8 @@
 # Fix /tmp/kafka-logs permissions
 chown -R kafka /tmp/kafka-logs
 
+sed -i -e 's@.*listeners=.*$@listeners=PLAINTEXT://'"$(hostname -f)"':9092@' /kafka/config/server.properties
+
 echo
 echo "== /kafka/config/server.properties =="
 cat /kafka/config/server.properties
